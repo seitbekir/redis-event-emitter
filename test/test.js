9f5188f195bd209759bdd3ee6b749a6c5d7b6def
Seitbekir <wided@ya.ru>
2016-04-07 16:21:54 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
+// create connection (you can use it in whole project, but store in file and export)
 var redisEventEmitter = require('../index.js')({
     host: 'redishost.com'
 });
 
 process.stdin.setEncoding('utf8');
 
+// Input data and press enter
 process.stdin.on('readable', function() {
 	var chunk = process.stdin.read();
 	if (chunk !== null) {
+		// On enter emit the message and the function
+		// to execute on another instance of this test program
 		redisEventEmitter.emit('mess', chunk, function(arg) {
 			console.log("Display: " + arg);
 		});
@@ -18,5 +22,6 @@ process.stdin.on('end', function() {
 });
 
 redisEventEmitter.on('mess', function(message, callback) {
+	// call function sent fron far server using current scope
 	callback(message);
 });
